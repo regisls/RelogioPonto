bd3d984ca1d209b1f64b1a7d570e5b3e04e072a1
Regis L. Sebastiani <regis@tecsistel.com.br>
2021-10-06 10:24:26 -0300

    Add project files.

---
+++ RelogioPonto/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace RelogioPonto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AppIcone = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuVerRegistros = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AppIcone
+            // 
+            this.AppIcone.ContextMenuStrip = this.menu;
+            this.AppIcone.Icon = ((System.Drawing.Icon)(resources.GetObject("AppIcone.Icon")));
+            this.AppIcone.Text = "Relógio ponto";
+            this.AppIcone.Visible = true;
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.mnuVerRegistros});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(139, 54);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // mnuVerRegistros
+            // 
+            this.mnuVerRegistros.Name = "mnuVerRegistros";
+            this.mnuVerRegistros.Size = new System.Drawing.Size(180, 22);
+            this.mnuVerRegistros.Text = "Ver registros";
+            this.mnuVerRegistros.Click += new System.EventHandler(this.mnuVerRegistros_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon AppIcone;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuVerRegistros;
+    }
+}
+
---
+++ RelogioPonto/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RelogioPonto
+{
+    public partial class Form1 : Form
+    {
+        private string BaseDir { get; set; }
+        private string AppDir { get; set; }
+        private string NomeArquivo { get; set; }
+        private string UltimoEvento { get; set; }
+
+        private const string SAIDA = "SAIDA";
+        private const string ENTRADA = "ENTRADA";
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            Hide();
+
+            this.BaseDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            this.AppDir = Path.Combine(BaseDir, "Folha Ponto");
+
+            if (!Directory.Exists(AppDir))
+                Directory.CreateDirectory(AppDir);
+
+            SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            SystemEvents.SessionEnding += new SessionEndingEventHandler(SystemEvents_SessionEnding);
+
+            string observacao = "Relógio Ponto iniciado";
+            string evento = ENTRADA;
+
+            GravarLog(evento, observacao);
+        }
+
+        private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        {
+            string observacao = string.Empty;
+            string evento = "";
+
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                //I left my desk
+                observacao = "Bloqueio do computador";
+                evento = SAIDA;
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                //I returned to my desk
+                observacao = "Desbloqueio do computador";
+                evento = ENTRADA;
+            }
+
+            GravarLog(evento, observacao);
+        }
+
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            string observacao = string.Empty;
+            string evento = SAIDA;
+
+            switch(e.Reason)
+            {
+                case SessionEndReasons.Logoff:
+                    observacao = "Logoff do usuário";
+                    break;
+                case SessionEndReasons.SystemShutdown:
+                    observacao = "Máquina desligada";
+                    break;
+            }
+
+            GravarLog(evento, observacao);
+        }
+
+        private void GravarLog(string Evento, string observacao = "")
+        {
+            this.NomeArquivo = Path.Combine(AppDir, $"Ponto-{DateTime.Now:yyyy-MM-dd}.txt");
+            this.UltimoEvento = $"{Evento,-7} | {DateTime.Now:HH:mm} | {observacao}";
+
+            File.AppendAllText(NomeArquivo, $"{UltimoEvento}\r\n");
+            
+            AppIcone.Text = $"Relógio ponto - {UltimoEvento}";
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AppIcone.Visible = false;
+            AppIcone.Dispose();
+            Application.Exit();
+        }
+
+        private void mnuVerRegistros_Click(object sender, EventArgs e)
+        {
+            Process.Start(AppDir);
+        }
+    }
+}
